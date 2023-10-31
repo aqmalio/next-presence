94d76b3e21414642d293090c64cde2a188f1efcc
aqmal.io <33825185+aqmalio@users.noreply.github.com>
2023-10-31 14:44:34 +0700

    Disable presence

--- app/page.tsx
+++ app/page.tsx
@@ -22,21 +22,21 @@ export default function Home() {
         redirect: 'follow'
       })
       const json = await res.json()
-      if (json.success) {
-        const res = await fetch(`${process.env.PRESENCE_API_BASEURL}tap_presensi?pegawai_id=${process.env.PRESENCE_NIP}&latitude=${formData.get('latitude')}&longitude=${formData.get('longitude')}&input_type=2&alamat=${process.env.PRESENCE_ALAMAT}&versi=2&imei=${process.env.PRESENCE_IMEI}&from=1`, {
-          cache: 'no-store',
-          method: 'POST',
-          headers: {
-            'user-agent': "Dart/2.18 (dart:io)",
-            'x-api-key': `${process.env.PRESENCE_API_X_API_KEY}`,
-            'Accept-Encoding': "gzip, deflate, br",
-            'token': `${process.env.PRESENCE_API_TOKEN}`,
-            'host': `${process.env.PRESENCE_API_HOST}`,
-            'Connection': "close"
-          },
-          redirect: 'follow'
-        })
-        const json = await res.json()
+      // if (json.success) {
+      //   const res = await fetch(`${process.env.PRESENCE_API_BASEURL}tap_presensi?pegawai_id=${process.env.PRESENCE_NIP}&latitude=${formData.get('latitude')}&longitude=${formData.get('longitude')}&input_type=2&alamat=${process.env.PRESENCE_ALAMAT}&versi=2&imei=${process.env.PRESENCE_IMEI}&from=1`, {
+      //     cache: 'no-store',
+      //     method: 'POST',
+      //     headers: {
+      //       'user-agent': "Dart/2.18 (dart:io)",
+      //       'x-api-key': `${process.env.PRESENCE_API_X_API_KEY}`,
+      //       'Accept-Encoding': "gzip, deflate, br",
+      //       'token': `${process.env.PRESENCE_API_TOKEN}`,
+      //       'host': `${process.env.PRESENCE_API_HOST}`,
+      //       'Connection': "close"
+      //     },
+      //     redirect: 'follow'
+      //   })
+      //   const json = await res.json()
       }
       return revalidatePath('/')
     } catch (error) {
